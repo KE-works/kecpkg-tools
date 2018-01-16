244cdbdd89c7033b4da90501306728e6fa1b2ec8
Jochem Berends <jochem.berends@ke-works.com>
2018-01-16 10:58:46 +0100

    included the `*-dist` and `_venv*` in the ignores.

--- kecpkg/settings.py
+++ kecpkg/settings.py
@@ -24,14 +24,14 @@ DEFAULT_SETTINGS = OrderedDict([
 ])
 
 EXCLUDE_DIRS_IN_BUILD = [
-    'venv', 'dist', '.idea', '.tox', '.cache', '.git', 'venv*', '.env', '__pycache__', 'develop-eggs', 'downloads',
-    'eggs', 'lib', 'lib64', 'sdist', 'wheels', '.hypothesis', '.ipynb_checkpoints', '.mypy_cache'
+    'venv', 'dist', '.idea', '.tox', '.cache', '.git', 'venv*', '_venv*', '.env', '__pycache__', 'develop-eggs',
+    'downloads', 'eggs', 'lib', 'lib64', 'sdist', 'wheels', '.hypothesis', '.ipynb_checkpoints', '.mypy_cache'
 ]
 
 EXCLUDE_PATHS_IN_BUILD = [
     '.gitignore', '*.pyc', '*.pyo', '*.pyd', '*$py.class', '*.egg-info', '.installed.cfg', '.coveragerc', '*.egg',
     'pip-log.txt', '*.log', 'pip-delete-this-directory.txt', '.coverage*', 'nosetests.xml', 'coverage.xml', '*.cover',
-    'env.bak', 'venv.bak', 'pip-selfcheck.json', '*.so'
+    'env.bak', 'venv.bak', 'pip-selfcheck.json', '*.so', '*-dist'
 ]
 
 EXCLUDE_IN_BUILD = EXCLUDE_DIRS_IN_BUILD + EXCLUDE_PATHS_IN_BUILD
