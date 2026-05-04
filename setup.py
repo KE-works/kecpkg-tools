b226548c6b246af0e1747a0f696cf5e35e56a436
Jochem Berends <jochem.berends@ke-works.com>
2026-05-04 15:06:31 +0200

    Refactor setup.py to use list syntax for keywords, classifiers, and install_requires. Updated entry_points format for consistency.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     maintainer_email="jochem.berends@ke-works.com",
     url="https://github.com/jberends/kecpkg-tools",
     license="Apache-2.0",
-    keywords=(
+    keywords=[
         "python",
         "package tools",
         "pykechain",
@@ -32,11 +32,10 @@ setup(
         "SIM",
         "KECPKG",
         "GPG",
-    ),
-    classifiers=(
+    ],
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.10",
@@ -46,22 +45,22 @@ setup(
         "Programming Language :: Python :: 3.14",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-    ),
+    ],
     python_requires=">=3.10",
-    install_requires=(
+    install_requires=[
         "click",
         "jinja2",
         "pykechain>=3.0",
         "platformdirs",
         "tabulate",
         "python-gnupg",
-    ),
+    ],
     packages=find_packages(exclude=["tests"]),
     # to include the templates in the bdist wheel, we need to add package_data here
     package_data={
         "kecpkg": ["files/templates/*.template", "files/templates/.*.template"]
     },
     entry_points={
-        "console_scripts": ("kecpkg = kecpkg.cli:kecpkg",),
+        "console_scripts": ["kecpkg = kecpkg.cli:kecpkg"],
     },
 )
